0f57c0d519e1cf8e967ae4ff2e3c51d81a397b00
vanessabridge <vane.m.bridge@gmail.com>
2021-01-14 23:48:08 -0600

    soporte para clasificar teclado sin endpoint

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -57,23 +57,26 @@ export default {
   },
   methods: {
     rateTweet(e) {
-      alert(`Tweet sent. tweetId ${this.tweetId} | isHistory ${this.isHistory}. Will load a new tweet!`)
-      this.getNextTweet()
+      alert(`Tweet sent. tweetId ${this.tweetId} | confidence ${this.confidence} | isHistory ${this.isHistory}. Will load a new tweet!`)
+      this.getRandomTweet()
       e.preventDefault()
     },
-    getNextTweet() {
+    getRandomTweet() {
       console.log("Sacando un random tweet mediante un API")
       const randomTweets = ["1341161863103488003", "1338243989561073664", "1349556594237792256", "1349496845635100672", "1349185664240283648", "1348787778591596545"]
 
       this.tweetId = randomTweets[Math.random() * randomTweets.length | 0]
+      this.confidence = 0
       this.isHistory = ''
+    },
+    doCommand(e) {
+    let cmd = String.fromCharCode(e.keyCode).toLowerCase();
+    if(cmd==='s'||cmd=='n'){
+      //De momento no hay endopoints que enviar pero una vez este listo se agrega a las actioins para si y no
+      alert(`you  pressed ${cmd}`)
     }
-  },
-  created() {
-    console.log("API CALL PARA SACAR ")
-    this.getNextTweet()
   }
-};
+  },
 </script>
 
 <style scoped lang="scss">
